82195b9f86d51ac453c94996c6a998eb6ccd02cd
brunolitrenta <brunovllitrenta2@gmail.com>
2025-12-11 23:07:46 -0300

    feat: add CORS configuration to enable cross-origin requests

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { HttpExceptionFilter } from './common/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors(
+    {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
